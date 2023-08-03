a13661de34af54981ad6ac47267f47f9bf629de3
Agustin <agusdolimpio@gmail.com>
2023-08-02 20:36:39 -0400

    conexion con backend exitosa

--- Login.js
+++ Login.js
@@ -1,28 +1,36 @@
-
-
 let btnLogin = document.getElementById("btnlogin");
 
-btnLogin.addEventListener("click", evento=>{
-    saveFields();
+btnLogin.addEventListener("click", evento => {
+    saveFields()
+        .then(message => {
+            console.log("Mensaje de éxito:", message);
+            alert("¡Registro exitoso!");
+        })
+        .catch(error => {
+            console.error("Error al registrar:", error);
+            alert("Error en el registro. Inténtalo nuevamente más tarde.");
+        });
 });
 
-let saveFields = async()=>{
+let saveFields = async () => {
+    let fields = {};
 
-let fields = {}
+    fields.username = document.getElementById("username").value;
+    fields.password = document.getElementById("password").value;
 
-fields.username = document.getElementsById("username").value;
-fields.password = document.getElementsById("password").value;
+    try {
+        const request = await fetch("http://localhost:8080/auth/login", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(fields)
+        });
 
-const request = await fetch("http://localhost:8080/auth/login",
-{
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify(fields)
-});
-
-
-
+        const response = await request.json();
+        return response.message; // Suponiendo que el servidor devuelve un objeto con una propiedad "message".
+    } catch (error) {
+        throw error;
+    }
 }
